f92d194a1a2da0f02fd099f4a6613e7b534d37eb
wwarriner <wwarriner@gmail.com>
2021-06-23 16:39:39 -0500

    improve output file structure

--- OutputFiles.m
+++ OutputFiles.m
@@ -5,26 +5,28 @@ classdef OutputFiles < handle
         end
         
         function write_slo_image(obj, image, subject_name)
-            parts = [string(subject_name) "slo"];
-            file = obj.generate_file(parts, ".tif");
+            parts = [string(subject_name) obj.SLO];
+            file = obj.generate_file(obj.SLO, parts, ".tif");
             imwrite(image, file);
         end
         
         function write_slo_rgb_image(obj, image, subject_name)
-            parts = [string(subject_name) "slo" "rgb"];
-            file = obj.generate_file(parts, ".tif");
+            parts = [string(subject_name) obj.SLO obj.RGB];
+            file = obj.generate_file(obj.SLO, parts, ".tif");
             imwrite(image, file);
         end
         
         function write_octa_image(obj, image, subject_name, image_index)
-            parts = [string(subject_name) "octa" num2str(image_index)];
-            file = obj.generate_file(parts, ".tif");
+            parts = [string(subject_name) obj.OCTA num2str(image_index)];
+            file = obj.generate_file(obj.OCTA, parts, ".tif");
             imwrite(image, file);
         end
         
         function write_octa_rgb_image(obj, image, subject_name, image_index)
-            parts = [string(subject_name) "octa" "rgb" num2str(image_index)];
-            file = obj.generate_file(parts, ".tif");
+            parts = [string(subject_name) obj.OCTA obj.RGB num2str(image_index)];
+            sub = [obj.OCTA, obj.RGB];
+            sub = strjoin(sub, "_");
+            file = obj.generate_file(obj.RGB, parts, ".tif");
             imwrite(image, file);
         end
     end
@@ -33,10 +35,18 @@ classdef OutputFiles < handle
         output_folder (1,1) string
     end
     
+    properties (Access = private, Constant)
+        SLO = "slo"
+        OCTA = "octa"
+        RGB = "rgb"
+    end
+    
     methods (Access = private)
-        function file = generate_file(obj, parts, ext)
+        function file = generate_file(obj, subfolder, parts, ext)
             name = obj.generate_name(parts, ext);
-            file = fullfile(obj.output_folder, name);
+            folder = fullfile(obj.output_folder, subfolder);
+            [~, ~] = mkdir(folder);
+            file = fullfile(folder, name);
         end
     end
     
