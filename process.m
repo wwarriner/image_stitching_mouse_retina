c86d76625998e3d0994cf8b1ec468e8874014604
wwarriner <wwarriner@gmail.com>
2021-06-28 14:26:07 -0500

    added printing, docs

--- process.m
+++ process.m
@@ -6,17 +6,39 @@ seeds = Seeds(fullfile(root_folder, "seeds.csv"));
 subject_count = ifi.get_subject_count();
 for subject = 1 : subject_count
     subject_name = ifi.get_subject_folder_name(subject);
+    fprintf("processing %s" + newline, subject_name);
     seed = seeds.get(subject_name);
+    
+    % OUTPUT SETUP
     output_folder = fullfile(root_folder, "..", "out", subject_name);
     ofi = OutputFiles(output_folder);
     
-    slo_images = ifi.read_slo_images(subject);
+    % SLO REGISTER
+    fprintf("  slo...");
+    try
+        slo_images = ifi.read_slo_images(subject);
+    catch
+        fprintf("can't read slo (check crop file), skipping" + newline);
+        continue;
+    end
+    if numel(slo_images) < 2
+        fprintf("only 1 volume, skipping..." + newline);
+        continue;
+    end
     transforms = register(slo_images, seed);
     [slo, slo_rgb] = combine(slo_images, transforms);
     ofi.write_slo_image(slo, subject_name);
     ofi.write_slo_rgb_image(slo_rgb, subject_name);
+    fprintf("done!" + newline);
     
-    offsets = ifi.read_offsets(subject);
+    % OCTA TRANSFORM
+    fprintf("  octa...");
+    try
+        offsets = ifi.read_offsets(subject);
+    catch
+        fprintf("can't read octa (check offset file), skipping" + newline);
+        continue;
+    end
     octa_transforms = translate_transforms(transforms, offsets);
     
     volume_count = ifi.get_volume_count(subject);
@@ -33,6 +55,8 @@ for subject = 1 : subject_count
         ofi.write_octa_image(octa, subject_name, image);
         ofi.write_octa_rgb_image(octa_rgb, subject_name, image);
     end
+    
+    fprintf("done!" + newline);
 end
 
 end
